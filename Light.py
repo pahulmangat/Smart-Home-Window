9a7e2c2f43a4b824a7247f14aa1e360bd15f1f2b
pahulmangat <70053740+pahulmangat@users.noreply.github.com>
2022-02-17 13:06:54 -0500

    Update Light.py

--- Light.py
+++ Light.py
@@ -2,10 +2,10 @@ import time
 import sys
 import os
 
+# import light sensor files
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'Capstone')
 if os.path.exists(libdir):
     sys.path.append(libdir)
-
 import logging
 import TSL2591
 
@@ -15,6 +15,7 @@ sensor = TSL2591.TSL2591()
 # sensor.SET_InterruptThreshold(0xff00, 0x0010)
 try:
     while True:
+        #print light sensor readings
         lux = sensor.Lux
         print('Lux: %d'%lux)
         sensor.TSL2591_SET_LuxInterrupt(50, 200)
@@ -23,8 +24,7 @@ try:
         visible = sensor.Read_Visible
         print('Visible light: %d'%visible)
         full_spectrum = sensor.Read_FullSpectrum
-        print('Full spectrum (IR + visible) light: %d\r\n'%full_spectrum)
-    
+        print('Full spectrum (IR + visible) light: %d\r\n'%full_spectrum)    
 except KeyboardInterrupt:    
     logging.info("ctrl + c:")
     sensor.Disable()
