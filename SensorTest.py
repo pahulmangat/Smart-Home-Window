63bf0a0901081426a262660af03099030ed32020
pahulmangat <70053740+pahulmangat@users.noreply.github.com>
2022-02-17 16:59:25 -0500

    Update SensorTest.py
    removed light sensor logging and file path reading

--- SensorTest.py
+++ SensorTest.py
@@ -6,23 +6,16 @@ import board
 import adafruit_dht
 
 # initialize light sensor
-libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'Capstone')
-if os.path.exists(libdir):
-    sys.path.append(libdir)
-import logging
 import TSL2591
-logging.basicConfig(level=logging.INFO)
 lightSensor = TSL2591.TSL2591()
-# sensor.SET_InterruptThreshold(0xff00, 0x0010)
 
 # Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT22(board.D5,use_pulseio=False)
+dhtDevice = adafruit_dht.DHT22(board.D4,use_pulseio=False)
 
 while True:
     try:
         #print light sensor readings
-        lux = lightSensor.Lux
-        print('Lux: %d'%lux)
+        print('Lux: %d'%lightSensor.Lux)
         lightSensor.TSL2591_SET_LuxInterrupt(50, 200)
         infrared = lightSensor.Read_Infrared
         print('Infrared light: %d'%infrared)
