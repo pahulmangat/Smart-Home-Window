f4fd867609b4c6693259140ced00b0635e4823a1
pahulmangat <70053740+pahulmangat@users.noreply.github.com>
2022-04-05 21:00:13 -0400

    Add files via upload

--- Rain.py
+++ Rain.py
@@ -1,1 +1,17 @@
+# Rain
+import RPi.GPIO as GPIO
+import time
+from pyrebase import pyrebase
+from pyfirmata import Arduino, util
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+Rain = 4
+
+board = Arduino('/dev/ttyACM0')
+
+while True:
+    isRain = board.digital[Rain].read
+    print("Rain:",isRain)
+    time.sleep(1)
